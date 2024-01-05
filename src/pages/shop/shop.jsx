a83e30daf3c2b0d237f6ef6e7c8a0cbe29f5686d
Rifadh <rifadhrfd@gmail.com>
2024-01-05 09:18:25 +0530

    Update shop name

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -7,7 +7,7 @@ export const Shop = () => {
   return (
     <div className="shop">
       <div className="shopTitle">
-      <h1>R_Tech Shop</h1>
+      <h1>Electronics</h1>
       </div>
       <div className="products">
         {" "}
